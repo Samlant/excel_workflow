c29f9f4ff591bf59d673d546115c363029fd51d6
Sam <7569798+Samlant@users.noreply.github.com>
2023-06-08 19:45:06 -0400

    Update dialog_entry_adder.py

--- dialog_entry_adder.py
+++ dialog_entry_adder.py
@@ -1,70 +1,65 @@
 from tkinter import *
-from tkinter import simpledialog
-
-
-class Assign_Dialog(simpledialog.Dialog):
-    def body(self, master):
-        Label(master, text="First:").grid(row=0, column=0)
-        Label(master, text="Second:").grid(row=1, column=0)
-
-        self.e1 = Entry(master)
-        self.e2 = Entry(master)
-
-        self.e1.grid(row=0, column=1)
-        self.e2.grid(row=1, column=1)
-        return self.e1  # initial focus
-
-    def apply(self):
-        first = self.e1.get()
-        second = self.e2.get()
-        print(first, second)
-
-
-class Action_Dialog(simpledialog.Dialog):
-    def body(self, master):
-        Label(
-            master, text="Would you like to allocate markets or submit to them?"
-        ).pack(fill=BOTH, expand=TRUE)
-
-    def buttonbox(self):
-        box = Frame(self)
-
-        w = Button(
-            box,
-            text="Submit to Markets",
-            width=30,
-            command=self.ok,
-            default=ACTIVE,
-        )
-        w.pack(side=LEFT, padx=5, pady=5)
-        w = Button(
-            box,
-            text="Allocate Markets",
-            width=30,
-            command=self.allocate,
-            default=ACTIVE,
-        )
-        w.pack(side=LEFT, padx=5, pady=5)
-        w = Button(
-            box,
-            text="No",
-            width=10,
-            command=self.cancel,
-        )
-        w.pack(side=LEFT, padx=5, pady=5)
-
-        self.bind("<Return>", self.ok)
-        self.bind("<Escape>", self.cancel)
-
-        box.pack()
-
-    def allocate(self):
-        print("Running allocate script")
-
-    # def apply(self):
-    #     print(w.result)
+import subprocess
+import os
 
 
 root = Tk()
-d = Action_Dialog(root)
-print(d.result)
+
+root.geometry("250x150")
+root.title("How to Proceed?")
+
+frame = Frame(root)
+frame.pack(fill=BOTH, expand=True)
+
+
+def create_folder():
+    file_name = "tba"  # not implemented
+    parent_dir = "tba"  # not decided
+    directory_name = file_name
+    path = os.path.join(parent_dir, directory_name)
+    os.makedirs(path)
+
+
+def allocate():
+    pass
+
+
+def run_quickdraw_app():
+    subprocess.run(["QuickDraw.exe"])
+
+
+def choice(option: str) -> None:
+    if option == "pass":
+        create_folder()
+    elif option == "allocate":
+        create_folder()
+        allocate_market()
+    else:
+        create_folder()
+        run_quickdraw_app()
+
+
+submit_btn = Button(
+    frame,
+    text="Submit to Markets",
+    width=30,
+    command=lambda: choice("submit"),
+    default=ACTIVE,
+)
+submit_btn.pack(side=LEFT, padx=5, pady=5)
+
+allocate_btn = Button(
+    frame,
+    text="Allocate Markets",
+    width=30,
+    command=lambda: choice("allocate"),
+)
+allocate_btn.pack(fill=BOTH, expand=True)
+
+pass_btn = Button(
+    frame,
+    text="Only create folder",
+    width=30,
+    command=lambda: choice("pass"),
+)
+pass_btn.pack(fill=BOTH, expand=True)
